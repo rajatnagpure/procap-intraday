ce008e85ae6d5802923ddba33863f098932ae607
Rajat Nagpure <rajatnagpureiiest@gmail.com>
2020-04-11 06:59:43 +0530

    value extraction success

--- extractvalues.py
+++ extractvalues.py
@@ -1,3 +1,15 @@
+from nsetools import Nse
+from constants import *
+
+
+def get_all_matches(input_list, subs):
+    res = []
+    for i in input_list:
+        if subs in i:
+            res.append(i)
+    return res
+
+
 class extractValues:
     def __init__(self, row_call_detail):
         self.detail = row_call_detail[2]
@@ -32,6 +44,24 @@ class extractValues:
             self.exit_price = float(self.detail[exit_price_init_index + 1:exit_price_last_index])
         # some other detail
         self.call_action = row_call_detail[1]
+        # getting stock Symbol or Ticker out of company name
+        # it might be wrong and will return None if went wrong
+        # it uses function get_all_matches
+        close_matches = get_all_matches(all_company_quotes, self.company_name.lower().replace(" ", ""))
+        close_match_fin = []
+        for i in close_matches:
+            number = all_company_quotes.index(i)
+            if number % 2 != 0:
+                number = number - 1
+            close_match_fin.append(all_company_quotes[number])
+        self.mini = 100
+        self.my_stock = close_match_fin[0]
+        nse = Nse()
+        for i in close_match_fin:
+            q = abs(nse.get_quote(i)['lastPrice'] - self.order_price)
+            if q < self.mini:
+                self.mini = q
+                self.my_stock = i
 
     def reuse(self, row_call_detail):
         self.detail = row_call_detail[2]
@@ -66,12 +96,36 @@ class extractValues:
             self.exit_price = float(self.detail[exit_price_init_index + 1:exit_price_last_index])
         # some other detail
         self.call_action = row_call_detail[1]
+        # getting stock Symbol or Ticker out of company name
+        # it might be wrong and will return None if went wrong
+        # it uses function get_all_matches
+        close_matches = get_all_matches(all_company_quotes, self.company_name.lower().replace(" ", ""))
+        close_match_fin = []
+        for i in close_matches:
+            number = all_company_quotes.index(i)
+            if number % 2 != 0:
+                number = number - 1
+            close_match_fin.append(all_company_quotes[number])
+        self.mini = 100
+        self.my_stock = close_match_fin[0]
+        nse = Nse()
+        for i in close_match_fin:
+            q = abs(nse.get_quote(i)['lastPrice'] - self.order_price)
+            if q < self.mini:
+                self.mini = q
+                self.my_stock = i
 
     def get_call_action(self):
         return self.call_action
 
     def get_company_name(self):
-        return self.company_name
+        if self.mini < 6:
+            return self.company_name
+        else:
+            return None
+
+    def get_company_quote(self):
+        return self.my_stock
 
     def get_order_price(self):
         return self.order_price
